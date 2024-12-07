63d2f135a91b8a71ee1eeb16bad2130aecbf152a
Daniel Skalski <daniel.skalski@gmail.com>
2024-12-07 23:31:46 +0100

    Day 2 part 1 solution

---
+++ day2/Program.cs
@@ -0,0 +1,28 @@
+﻿var file = File.OpenText("input.txt");
+
+var safeReports = 0;
+
+while (file.ReadLine() is { } line)
+{
+    var report = line.Split(" ").Select(int.Parse).ToArray();
+
+    var direction = report[0] - report[1] < 0;
+    var isSafe = true;
+    for (int i = 0, j = 1; j < report.Length; i++, j++)
+    {
+        var diff = report[i] - report[j];
+        var currDirection = diff < 0;
+        isSafe = currDirection == direction && (1 <= Math.Abs(diff) && Math.Abs(diff) <= 3);
+        if (!isSafe)
+        {
+            break;
+        }
+    }
+
+    if (isSafe)
+    {
+        safeReports++;
+    }
+}
+
+Console.WriteLine(safeReports);
