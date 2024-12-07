35f4eaa83944a2762e453095776d8b15bd8f16f6
Daniel Skalski <daniel.skalski@gmail.com>
2024-12-07 22:30:28 +0100

    Day 1 part 1 solution

---
+++ day1/Program.cs
@@ -0,0 +1,15 @@
+﻿using var file = File.OpenText("input.txt");
+
+List<int> list1 = [];
+List<int> list2 = [];
+
+while (file.ReadLine() is {} line)
+{
+    var numbers = line.Split(" ");
+    list1.Add(int.Parse(numbers[0]));
+    list2.Add(int.Parse(numbers[^1]));
+}
+
+var result = list1.Order().Zip(list2.Order(), (a, b) => Math.Abs(a - b)).Sum();
+
+Console.WriteLine(result);
