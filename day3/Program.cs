94060f15777e5b289e6db94350596337f21851fd
Daniel Skalski <daniel.skalski@gmail.com>
2024-12-08 01:29:57 +0100

    Day 3 part 1 solution

---
+++ day3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+var file = File.OpenText("input.txt");
+
+var text = file.ReadToEnd();
+
+const string pattern = @"mul\(\d+,\d+\)";
+
+var sum = 0;
+foreach (Match match in Regex.Matches(text, pattern))
+{
+    var nums = match.Value[4..^1].Split(',').Select(int.Parse).ToArray();
+    sum += nums[0] * nums[1];
+}
+
+Console.WriteLine(sum);
