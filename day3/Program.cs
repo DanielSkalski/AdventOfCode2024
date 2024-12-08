6591f60671730d0363318bf26500b4ef0c651174
Daniel Skalski <daniel.skalski@gmail.com>
2024-12-08 01:50:50 +0100

    Day 3 part 2 solution

--- day3/Program.cs
+++ day3/Program.cs
@@ -5,12 +5,24 @@ var file = File.OpenText("input.txt");
 var text = file.ReadToEnd();
 
 const string pattern = @"mul\(\d+,\d+\)";
+const string doPattern = @"do\(\)";
+const string dontPattern = @"don\'t\(\)";
+
+var disableIndexes = Regex.Matches(text, dontPattern).Select(x => (int?)x.Index).ToArray();
+var enableIndexes = Regex.Matches(text, doPattern).Select(x => x.Index).ToArray();
 
 var sum = 0;
 foreach (Match match in Regex.Matches(text, pattern))
 {
-    var nums = match.Value[4..^1].Split(',').Select(int.Parse).ToArray();
-    sum += nums[0] * nums[1];
+    var nearestDisableIndex = disableIndexes.LastOrDefault(x => x < match.Index) ?? -1;
+    var nearestEnableIndex = enableIndexes.LastOrDefault(x => x < match.Index);
+
+    var isEnabled = nearestEnableIndex > nearestDisableIndex;
+    if (isEnabled)
+    {
+        var nums = match.Value[4..^1].Split(',').Select(int.Parse).ToArray();
+        sum += nums[0] * nums[1];
+    }
 }
 
 Console.WriteLine(sum);
