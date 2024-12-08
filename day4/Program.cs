901f488bbdc6bcbb0c2e42439cdc399848cf0da5
Daniel Skalski <daniel.skalski@gmail.com>
2024-12-08 23:01:37 +0100

    Day 4 part 2 solution

--- day4/Program.cs
+++ day4/Program.cs
@@ -7,76 +7,19 @@ while (file.ReadLine() is { } line)
     lines.Add(line);
 }
 
-int wordsCount = 0;
+var wordsCount = 0;
 
-for (int i = 0; i < lines.Count; i++)
+for (var i = 1; i < lines.Count - 1; i++)
 {
     var currLine = lines[i];
-    for (int j = 0; j < currLine.Length; j++)
+    for (var j = 1; j < currLine.Length - 1; j++)
     {
-        if (currLine[j] == 'X')
+        if (currLine[j] == 'A')
         {
-            if (j >= 3)
+            if (((lines[i - 1][j - 1] == 'M' && lines[i + 1][j + 1] == 'S') || (lines[i - 1][j - 1] == 'S' && lines[i + 1][j + 1] == 'M')) &&
+                ((lines[i + 1][j - 1] == 'M' && lines[i - 1][j + 1] == 'S') || (lines[i + 1][j - 1] == 'S' && lines[i - 1][j + 1] == 'M')))
             {
-                if (currLine[j - 1] == 'M' && currLine[j - 2] == 'A' && currLine[j - 3] == 'S')
-                {
-                    wordsCount++;
-                }
-            }
-            if (j < currLine.Length - 3)
-            {
-                if (currLine[j + 1] == 'M' && currLine[j + 2] == 'A' && currLine[j + 3] == 'S')
-                {
-                    wordsCount++;
-                }
-            }
-
-            if (i >= 3)
-            {
-                if (lines[i - 1][j] == 'M' && lines[i - 2][j] == 'A' && lines[i - 3][j] == 'S')
-                {
-                    wordsCount++;
-                }
-            }
-
-            if (i < lines.Count - 3)
-            {
-                if (lines[i + 1][j] == 'M' && lines[i + 2][j] == 'A' && lines[i + 3][j] == 'S')
-                {
-                    wordsCount++;
-                }
-            }
-
-            if (j >= 3 && i >= 3)
-            {
-                if (lines[i - 1][j - 1] == 'M' && lines[i - 2][j - 2] == 'A' && lines[i - 3][j - 3] == 'S')
-                {
-                    wordsCount++;
-                }
-            }
-
-            if (j >= 3 && i < lines.Count - 3)
-            {
-                if (lines[i + 1][j - 1] == 'M' && lines[i + 2][j - 2] == 'A' && lines[i + 3][j - 3] == 'S')
-                {
-                    wordsCount++;
-                }
-            }
-
-            if (j < currLine.Length - 3 && i >= 3)
-            {
-                if (lines[i - 1][j + 1] == 'M' && lines[i - 2][j + 2] == 'A' && lines[i - 3][j + 3] == 'S')
-                {
-                    wordsCount++;
-                }
-            }
-
-            if (j < currLine.Length - 3 && i < lines.Count - 3)
-            {
-                if (lines[i + 1][j + 1] == 'M' && lines[i + 2][j + 2] == 'A' && lines[i + 3][j + 3] == 'S')
-                {
-                    wordsCount++;
-                }
+                wordsCount++;
             }
         }
     }
